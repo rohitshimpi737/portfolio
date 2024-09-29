980c817d8e4e83b0447c23396e375625dc335488
Rohit Vilas Shimpi <118548989+rohitshimpi737@users.noreply.github.com>
2024-09-30 01:43:53 +0530

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -107,7 +107,7 @@ function showProjects(projects) {
         projectHTML += `
         <div class="box tilt">
       // <img draggable="false" src="/assets/images/projects/${project.image}.png" alt="project" />
-    <img draggable="false" src="/assets/images/projects/EduHub.png" alt="project" />
+    <img draggable="false" src="./assets/images/projects/EduHub.png" alt="project" />
       <div class="content">
         <div class="tag">
         <h3>${project.name}</h3>
